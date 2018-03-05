0ae86474036a3dc957c75798b18aedf110ece4a9
obama <pvd02091992@gmail.com>
2018-03-05 15:10:22 +0700

    update css import component

--- next.config.js
+++ next.config.js
@@ -1,23 +1,35 @@
 require('dotenv').config({silent: true});
 const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+const ExtractTextPlugin      = require('extract-text-webpack-plugin');
 const path                   = require('path');
 const {ANALYZE}              = process.env;
 const env                    = require('./env-config');
 const webpack                = require('webpack');
 const ROOT_DIR               = path.resolve(__dirname);
-const withCSS                = require('@zeit/next-css');
-module.exports               = withCSS();
+// const withCSS                = require('@zeit/next-css');
+// module.exports               = withCSS();
+const extractCSS = new ExtractTextPlugin({filename: 'style.css', disable: false, allChunks: true});
 
 module.exports = {
     distDir            : 'public',
     pageExtensions     : ['jsx', 'js'],
     webpack            : (config, {isServer}) => {
+        config.performance.hints = false;
         config.module.rules.push(
             {
-                test  : /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
+                test: /\.css$/,
+                use : extractCSS.extract({
+                    fallback: 'style-loader',
+                    use     : [
+                        'css-loader', {
+                            loader: 'string-replace-loader',
+                            query : {multiple: []},
+                        },
+                    ],
+                }),
+            },
         );
+        config.plugins.push(extractCSS);
         config.resolve['alias']['service'] = `${ROOT_DIR}/service/`;
         config.plugins.push(new webpack.EnvironmentPlugin(env));
         if (ANALYZE === 'true') {
