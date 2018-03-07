8925225fc0a058a4d6686473ab3c71ca39da60fa
obama <pvd02091992@gmail.com>
2018-03-07 10:27:28 +0700

    css

--- next.config.js
+++ next.config.js
@@ -6,31 +6,16 @@ const {ANALYZE}              = process.env;
 const env                    = require('./env-config');
 const webpack                = require('webpack');
 const ROOT_DIR               = path.resolve(__dirname);
-// const withCSS                = require('@zeit/next-css');
-// module.exports               = withCSS();
-const extractCSS = new ExtractTextPlugin({filename: 'style.css', disable: false, allChunks: true});
 
 module.exports = {
     distDir            : 'public',
     pageExtensions     : ['jsx', 'js'],
-    webpack            : (config, {isServer}) => {
-        config.performance.hints = false;
-        config.module.rules.push(
-            {
-                test: /\.css$/,
-                use : extractCSS.extract({
-                    fallback: 'style-loader',
-                    use     : [
-                        'css-loader', {
-                            loader: 'string-replace-loader',
-                            query : {multiple: []},
-                        },
-                    ],
-                }),
-            },
-        );
-        config.plugins.push(extractCSS);
-        config.resolve['alias']['service'] = `${ROOT_DIR}/service/`;
+    webpack            : (config, options) => {
+        const {dev, isServer} = options;
+        if (!dev) {
+            config.devtool = 'source-map'
+        }
+        //rules
         config.plugins.push(new webpack.EnvironmentPlugin(env));
         if (ANALYZE === 'true') {
             config.plugins.push(new BundleAnalyzerPlugin({
@@ -39,13 +24,9 @@ module.exports = {
                 openAnalyzer: true
             }))
         }
-        console.log(config.module.rules);
+        //alias folder
+        config.resolve['alias']['service'] = `${ROOT_DIR}/service/`;
+        config.performance.hints           = false;
         return config
     },
-    serverRuntimeConfig: { // Will only be available on the server side
-        mySecret: 'secret'
-    },
-    publicRuntimeConfig: { // Will be available on both server and client
-        staticFolder: '/public'
-    }
 };
--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {AuthService} from 'service';
-import './about.css';
 
 class About extends React.Component {
     constructor(props) {
@@ -18,8 +17,19 @@ class About extends React.Component {
         });
         return (
             <div>
-                <h1 className="about">About page {this.state.name}</h1>
-                <span className="dinhceo">dinhceo</span>
+                <h1 className="about">About page</h1>
+                <h1 className="about">State page {this.state.name}</h1>
+                <style jsx>
+                    {`
+                        .about {
+                            font: 15px Helvetica, Arial, sans-serif;
+                            background: #eee;
+                            padding: 20px;
+                            text-align: center;
+                            transition: 100ms ease-in background;
+                        }
+                    `}
+                </style>
             </div>
         )
     }
