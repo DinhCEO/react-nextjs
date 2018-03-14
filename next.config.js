f35b864feacf4f8faa0de648c304e15813adf10b
obama <pvd02091992@gmail.com>
2018-03-14 23:50:14 +0700

    update nextjs config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,13 @@
 require('dotenv').config({silent: true});
 const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
-const CompressionPlugin      = require('compression-webpack-plugin');
 const path                   = require('path');
 const {ANALYZE}              = process.env;
 const env                    = require('./env-config');
 const webpack                = require('webpack');
 const ROOT_DIR               = path.resolve(__dirname);
-const BUILD_DIR              = ROOT_DIR + '/abc';
 
 module.exports = {
-    distDir       : 'public',
-    pageExtensions: ['jsx', 'js'],
     webpack       : (config, options) => {
-        process.env.languages.split(',').map(lang => buildConfigByLanguage(lang, config));
         const {dev, isServer} = options;
         if (!dev) {
             config.devtool = 'source-map'
@@ -33,67 +28,3 @@ module.exports = {
         return config;
     },
 };
-
-function buildConfigByLanguage(lang, config) {
-    // const entryFactory       = config.entry;
-    // config.entry             = () => (
-    //     entryFactory().then((entry) => {
-    //         entry['language'] = ROOT_DIR + `/i18n/${lang}.js`;
-    //         return entry;
-    //     })
-    // );
-    // config.output['library'] = 'MyTranslator';
-    // config.module.rules.push({
-    //     test   : /\.js?/,
-    //     exclude: /node_modules/,
-    //     include: ROOT_DIR,
-    //     use    : {loader: 'babel-loader'},
-    // });
-    // config.plugins.push(
-    //     new webpack.optimize.UglifyJsPlugin({
-    //         minimize: true,
-    //         compress: {warnings: false},
-    //         output  : {comments: false},
-    //     }),
-    //     new webpack.optimize.OccurrenceOrderPlugin(),
-    //     new CompressionPlugin({
-    //         algorithm: 'gzip',
-    //         threshold: 0,
-    //     })
-    // );
-    const configLanguage = {
-        entry  : {
-            language: ROOT_DIR + `/i18n/${lang}.js`,
-        },
-        output : {
-            path         : BUILD_DIR,
-            filename     : `[name]-${lang}.min.js`,
-            libraryTarget: 'umd',
-            library      : 'MyTranslator',
-        },
-        module : {
-            rules: [
-                {
-                    test   : /\.js?/,
-                    exclude: /node_modules/,
-                    include: ROOT_DIR,
-                    use    : {loader: 'babel-loader'},
-                },
-            ],
-        },
-        plugins: [
-            new webpack.optimize.UglifyJsPlugin({
-                minimize: true,
-                compress: {warnings: false},
-                output  : {comments: false},
-            }),
-            new webpack.optimize.OccurrenceOrderPlugin(),
-            new CompressionPlugin({
-                algorithm: 'gzip',
-                threshold: 0,
-            })
-        ],
-
-    };
-    return [configLanguage];
-}
--- pages/home/index.js
+++ pages/home/index.js
@@ -11,18 +11,18 @@ class Index extends React.Component {
     render() {
         return (
             <div>
-                <h1 className="about">About page {this.state.name}</h1>
-                {/*<style jsx>*/}
-                    {/*{`*/}
-                        {/*.about {*/}
-                            {/*font: 15px Helvetica, Arial, sans-serif;*/}
-                            {/*background: #eee;*/}
-                            {/*padding: 20px;*/}
-                            {/*text-align: center;*/}
-                            {/*transition: 100ms ease-in background;*/}
-                        {/*}*/}
-                    {/*`}*/}
-                {/*</style>*/}
+                <h1 className="about">/home page {this.state.name}</h1>
+                <style jsx>
+                    {`
+                        .about {
+                            font: 15px Helvetica, Arial, sans-serif;
+                            background: #eee;
+                            padding: 20px;
+                            text-align: center;
+                            transition: 100ms ease-in background;
+                        }
+                    `}
+                </style>
             </div>
         )
     }
